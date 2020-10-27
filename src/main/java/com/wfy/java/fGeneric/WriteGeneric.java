a308a5dc6808dbe63fc38cc11c96ca0ced943ed0
wfy0508 <fayin1004@gmail.com>
2020-10-27 17:55:50 +0800

    add GenericDefine 20201027

---
+++ src/main/java/com/wfy/java/fGeneric/WriteGeneric.java
@@ -0,0 +1,13 @@
+package com.wfy.java.fGeneric;
+
+/*
+ * @Author wfy
+ * @Date 2020/10/27 15:34
+ * com.wfy.java.fGeneric
+ */
+
+public class WriteGeneric {
+    public static void main(String[] args) {
+
+    }
+}
